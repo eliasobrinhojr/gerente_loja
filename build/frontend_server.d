53f66347adb7fc2a083c0658a2fac8271fb205bd
elias <araujo>
2019-08-14 16:17:18 -0400

    initial commit

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /home/sys00/Documentos/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,79 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/user_bloc.dart';
+import 'package:gerente_loja/tabs/orders_tab.dart';
+import 'package:gerente_loja/tabs/users_tab.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController;
+  int _page = 0;
+
+  UserBloc _userBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    _userBloc = UserBloc();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Colors.pinkAccent,
+            primaryColor: Colors.white,
+            textTheme: Theme.of(context)
+                .textTheme
+                .copyWith(caption: TextStyle(color: Colors.white54))),
+        child: BottomNavigationBar(
+            currentIndex: _page,
+            onTap: (p) {
+              _pageController.animateToPage(p,
+                  duration: Duration(milliseconds: 500), curve: Curves.ease);
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), title: Text("Clientes")),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.shopping_cart), title: Text("Pedidos")),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.list), title: Text("Produtos")),
+            ]),
+      ),
+      body: SafeArea(
+        child: BlocProvider<UserBloc>(
+          bloc: _userBloc,
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: (p) {
+              setState(() {
+                _page = p;
+              });
+            },
+            children: <Widget>[
+              UsersTab(),
+              OrdersTab(),
+              Container(
+                color: Colors.green,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+}
